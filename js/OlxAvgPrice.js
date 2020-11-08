32debce72ed01661f2c0ac2e7b6e6af46949fe3c
Dawid Stronczak <dstronczak@guidewire.com>
2020-11-08 11:52:50 +0100

    add first script to count avg price for olx search results

---
+++ js/OlxAvgPrice.js
@@ -0,0 +1,30 @@
+var styles = [
+    'background: linear-gradient(#D33106, #571402)'
+    , 'border: 1px solid #3E0E02'
+    , 'color: white'
+    , 'display: block'
+    , 'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)'
+    , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+    , 'line-height: 40px'
+    , 'text-align: center'
+    , 'font-weight: bold'
+].join(';');
+
+function counAvgPrice(){
+    var els = document.getElementsByClassName("price");
+    var count=0; var total=0;
+    Array.prototype.forEach.call(els, function(el) {
+        // Do stuff here
+        if(el.childNodes[1]){
+            var priceText = el.childNodes[1].innerHTML;
+            var priceVal = parseInt(priceText);
+            if(!isNaN(priceVal)){
+                count++;
+                total+=priceVal;
+            }
+        }
+    });
+    console.log('%c Average price of items on the page: %s', styles, total/count);
+}
+
+counAvgPrice();
